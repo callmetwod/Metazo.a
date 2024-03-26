f3bdd6a2cc8ccf4381f14b6f56cc5f6ef7649e2a
David <d4vidmenezes@gmail.com>
2024-03-26 11:00:30 -0300

    chore: some lines in main.jsx are deleted and index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Home from './views/home/home'
+import Card from './components/card/card.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Card/>
   </React.StrictMode>,
-  )
+)
