70612cf11be46012e0274f98804b98d10c76e20a
callmetwod <d4vidmenezes@gmail.com>
2024-03-30 00:29:55 -0300

    feat(components): added cardSlider component

---
+++ src/components/cardsSlider/cardsSlider.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react"
+import { useState } from 'react'
+import Card from '../card/card'
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Pagination, Navigation } from "swiper/modules";
+
+export default function CardsSlider(params) {
+
+    const url = `https://metazoa-api.onrender.com/soss`
+
+    const [users, setUsers] = useState([])
+
+    async function handleUsers() {
+        try {
+            const response = (await axios.get(url)).data.SOSs
+            console.log(response)
+            setUsers(response)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function showUsers() {
+        await users
+        console.log(users)
+    }
+
+    function deleteUsers() {
+        setUsers([])
+    }
+
+    return (
+        <div className="cards">
+            <Swiper
+                slidesPerView={6}
+                effect="coverflow"
+                grabCursor={true}
+                // pagination={{
+                //     dynamicMainBullets: true,
+                //     type: "bullets"
+                // }}
+                /* height: 100%; */
+
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                {<div swipers>
+                    {
+                        users.map(function (iten) {
+                            return (
+                                <SwiperSlide key={iten.id}><Card name={iten.name} email={iten.email} created_at={iten.createdAt} /></SwiperSlide>
+
+                            )
+                        })
+                    }
+                </div>}
+            </Swiper>
+        </div>
+    )
+}
+
+
