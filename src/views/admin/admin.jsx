600d87caa6830696a906d1a47c3696a3b3b27d7e
callmetwod <d4vidmenezes@gmail.com>
2024-03-30 00:25:14 -0300

    feat(views): added admin page

--- src/views/admin/admin.jsx
+++ src/views/admin/admin.jsx
@@ -1,18 +1,89 @@
-import Card from '../../components/card/card'
-import ProfileBar from '../../components/profileBar/profileBar'
+import logo from '../../assets/logo/white.svg'
 import './admin.css'
 
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react"
+import { useState } from 'react'
+import Card from '../../components/card/card'
+import Button from '../../components/button/button';
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 export default function Admin() {
-    return(
+
+    const [data, setData] = useState([])
+
+    async function showSOSs() {
+        try {
+            const response = (await axios.get('https://metazoa-api.onrender.com/soss')).data.SOSs
+            console.log(response)
+            setData(response)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function showUsers() {
+        try {
+            const response = (await axios.get('https://metazoa-api.onrender.com/users')).data.allUsers
+            console.log(response)
+            setData(response)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function showProfessionals() {
+        try {
+            const response = (await axios.get('https://metazoa-api.onrender.com/professionals')).data.professionals
+            console.log(response)
+            setData(response)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function showData() {
+        console.log(data)
+    }
+
+    return (
         <div className="admin">
-            <div className="profile">
-                <ProfileBar name={'David'}/>
-            </div>
-            <div className="container-main">
-                <Card/>
-            </div>
-            <div className="footer"></div>
+            <img id='logo' src={logo} alt="white logo" />
+            <body>
+                <div id="categories">
+                    <div className="admin-categories">
+                        <div className="title"><h1>Vizualizar Dados</h1></div>
+                        <div className="categories-buttons">
+                            <Button function={showUsers} title={'Users'} />
+                            <Button function={showProfessionals} title={'Professionals'} />
+                            <Button function={showSOSs} title={'SOSs'} />
+                            {/* <Button function={showData} title={'Mostrar Data'} /> */}
+                        </div>
+                    </div>
+                </div>
+                <div className="cards">
+                    <Swiper
+                        slidesPerView={6}
+                        effect="coverflow"
+                        grabCursor={true}
+                        className="mySwiper"
+                    >
+                        {<div swipers>
+                            {
+                                data.map(function (iten) {
+                                    return (
+                                        <SwiperSlide key={iten.id}><Card name={iten.name} email={iten.email} created_at={iten.createdAt} /></SwiperSlide>
+
+                                    )
+                                })
+                            }
+                        </div>}
+                    </Swiper>
+                </div>
+            </body>
         </div>
     )
 }
