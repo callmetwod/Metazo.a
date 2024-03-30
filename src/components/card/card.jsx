60dff31ea750ca6ffcced06c059db4349281037c
callmetwod <d4vidmenezes@gmail.com>
2024-03-30 00:23:52 -0300

    feat(components): added card components

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,19 +2,21 @@ import ProfileIcon from '../profileIcon/profileIcon'
 import './card.css'
 import CardProperties from './cardProperties/cardProperties'
 
-export default function Card(){
+export default function Card(props) {
     return (
-        <div className="card">
-            <div className="profile">
-                <ProfileIcon
-                width={'5rem'} 
-                height={'5rem'} 
-                margin={'auto'}
-                margin-bottom={'20px'}
-                />
-                <CardProperties property={'Name:'} value={'Deivide E A Menezes'}/>
-                <CardProperties property={'Email:'} value={'Deivide E A Menezes'}/>
-                <CardProperties property={'Password:'} value={'Deivide E A Menezes'}/>
+        <div className="card" style={{ ...props }}>
+            <div className="profile-properties">
+                <div className="icon">
+                    <ProfileIcon
+                        width={'5rem'}
+                        height={'5rem'}
+                        margin={'auto'}
+                        marginBottom={'20px'}
+                    />
+                </div>
+                <CardProperties property={'Name:'} value={props.name} />
+                <CardProperties property={'Email:'} value={props.email} />
+                <CardProperties property={'Created At:'} value={props.created_at} />
             </div>
         </div>
     )
